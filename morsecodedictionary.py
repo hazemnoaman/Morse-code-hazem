94f7f3736ccbc21290b3599030a55a8fed369085
hazemnoaman <145559445+hazemnoaman@users.noreply.github.com>
2023-11-29 01:50:01 +0200

    Update morsecodedictionary.py
    this is the encrypt and decrypt functions

--- morsecodedictionary.py
+++ morsecodedictionary.py
@@ -19,3 +19,27 @@ morse_code_dict = {
     '9': '----.', '0': '-----',
     ' ': '/'
 }
+def encrypt(text):
+    """Encrypts a given English phrase into Morse code."""
+    encrypted_text = ''
+    for char in text.upper():
+        if char in morse_code_dict:
+            encrypted_text += morse_code_dict[char] + ' '
+        else:
+            encrypted_text += ' '
+
+    return encrypted_text.strip()
+
+def decrypt(morse_code):
+    """Decrypts Morse code into an English phrase."""
+    morse_code_words = morse_code.split(' ')
+    decrypted_text = ''
+    for word in morse_code_words:
+        for key, value in morse_code_dict.items():
+            if value == word:
+                decrypted_text += key
+                break
+        else:
+            decrypted_text += ' '
+
+    return decrypted_text
